6a0a03d8cf9b4e9eb288b011b4faaa4608e69a36
pacino9515 <1652258118@qq.com>
2017-09-25 21:06:04 +0800

    修改玩家配比页面
    逻辑关系

--- task2/style.js
+++ task2/style.js
@@ -2,6 +2,11 @@
 
 var playerNum =document.getElementById("playerNum");
 var slider =document.getElementById("slider");
+var ghostNum =document.getElementById("numY");
+var peopleNum =document.getElementById("numS");
+
+
+
 function onChange(){//输入框人数同步修改滑块
 	if(playerNum.value>=6&&playerNum.value<=18){
 		slider.value=playerNum.value;
@@ -10,10 +15,17 @@ function onChange(){//输入框人数同步修改滑块
 		playerNum.value=6;
 		slider.value=6;
 	}
+	ghostNum.value=parseInt((playerNum.value/3));
+
+	peopleNum.value=playerNum.value-ghostNum.value;
 
 }
 function moveChange(){//滑块数值同步修改输入框
 	playerNum.value=slider.value;
+	ghostNum.value=parseInt((playerNum.value/3));
+
+	peopleNum.value=playerNum.value-ghostNum.value;
+
 
 }
 function reduce(){
@@ -23,6 +35,9 @@ function reduce(){
 	}else{
 		slider.value=playerNum.value;
 	}
+	ghostNum.value=parseInt((playerNum.value/3));
+
+	peopleNum.value=playerNum.value-ghostNum.value;
 }
 function plus(){
 	playerNum.value++;
@@ -32,5 +47,12 @@ function plus(){
 	}else{
 		slider.value=playerNum.value;
 	}
+	ghostNum.value=parseInt((playerNum.value/3));
+
+	peopleNum.value=playerNum.value-ghostNum.value;
 
 }
+function link(){
+	window.location.href="peibi.html";
+}
+
