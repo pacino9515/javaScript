54209c8c575b3e03dc8b9334908e626ad5f793f5
pacino9515 <1652258118@qq.com>
2017-09-26 22:39:14 +0800

    完成翻牌页面和部分逻辑部分

--- task2/style.js
+++ task2/style.js
@@ -4,6 +4,9 @@ var playerNum =document.getElementById("playerNum");
 var slider =document.getElementById("slider");
 var ghostNum =document.getElementById("numY");
 var peopleNum =document.getElementById("numS");
+var kuangOne=document.getElementById("kuangone")
+var kuangTwo=document.getElementById("kuangtwo")
+
 
 
 
@@ -55,4 +58,45 @@ function plus(){
 function link(){
 	window.location.href="peibi.html";
 }
+
+var array=[];
+function buildArray(){//创建数组
+	for(var i=0;i<(ghostNum.value);i++){
+		array.push("幽灵");
+	}
+	for(var j=0;j<peopleNum.value;j++){
+		array.push("水民")
+	}
+	//数组乱序
+
+	var arrayA = [];
+    while (array.length > 0) {
+        var index = parseInt(Math.random() * (array.length - 1));
+        arrayA.push(array[index]);
+        array.splice(index, 1);
+    }
+
+	//跳转
+	window.location.href="fanpai.html";
+
+}
+var change =true;
+function next(){
+	if(!change){
+		kuangOne.style.display="flex";
+		kuangTwo.style.display="none";
+	}else{
+		kuangTwo.style.display="flex";
+		kuangone.style.display="none";
+	}
+	change=!change;
+
+}
+
+
+// function show(){
+// 	var i=0;//第几号
+// 	var j=0;//用来判断处于何种状态 偶数XXX  奇数XX
+
+// }
 
