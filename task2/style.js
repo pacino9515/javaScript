7f5d313bd48628d1868431334d21aa1f9a6fb446
pacino9515 <1652258118@qq.com>
2017-10-02 22:41:43 +0800

    10.2修改页面
    使用localStorage存储变量和数组

--- task2/style.js
+++ task2/style.js
@@ -6,6 +6,8 @@ var ghostNum =document.getElementById("numY");
 var peopleNum =document.getElementById("numS");
 var kuangOne=document.getElementById("kuangone")
 var kuangTwo=document.getElementById("kuangtwo")
+var shuimingWord=document.getElementById("shuimingWord");
+var ghostWord=document.getElementById("ghostWord");
 
 
 
@@ -31,7 +33,7 @@ function moveChange(){//滑块数值同步修改输入框
 
 
 }
-function reduce(){
+function reduce(){ //左侧数值减少
 	playerNum.value--;
 	if(playerNum.value<6){
 		playerNum.value=6;
@@ -42,7 +44,7 @@ function reduce(){
 
 	peopleNum.value=playerNum.value-ghostNum.value;
 }
-function plus(){
+function plus(){//右侧数值增加
 	playerNum.value++;
 	if(playerNum.value>18){
 
@@ -54,26 +56,49 @@ function plus(){
 
 	peopleNum.value=playerNum.value-ghostNum.value;
 
+
 }
 function link(){
 	window.location.href="peibi.html";
 }
 
-var array=[];
+
+// var array=[];
+array=[];
+
 function buildArray(){//创建数组
+
+
 	for(var i=0;i<(ghostNum.value);i++){
 		array.push("幽灵");
 	}
 	for(var j=0;j<peopleNum.value;j++){
 		array.push("水民")
 	}
+
+
 	//数组乱序
-	var arrayA = [];
-    while (array.length > 0) {
-        var index = parseInt(Math.random() * (array.length - 1));
-        arrayA.push(array[index]);
-        array.splice(index, 1);
+	 var len = array.length;
+    for (var r = 0; r < len - 1; r++) {
+        var index = parseInt(Math.random() * (len - r));
+        var temp = array[index];
+        array[index] = array[len - r - 1];
+        array[len - r - 1] = temp;
     }
+    //检测浏览器是否支持
+    if(typeof(Storage) !== "undefined"){
+    	//先将数组转换成JSON对象再
+    	localStorage.setItem("lastname", JSON.stringify(array));//存入数组
+    	localStorage.setItem("playerNum",playerNum.value);//总玩家
+
+
+
+    }else{
+    	alert("抱歉你的浏览器不支持")
+    }
+
+
+
 
 
 	//跳转
@@ -83,6 +108,15 @@ function buildArray(){//创建数组
 var change =true;
 var NO=0;
 function next(){
+	// var array=JSON.parse(localStorage.getltem("key"))
+	var array=JSON.parse(localStorage.getItem("lastname"));
+	var playerNum=localStorage.getItem("playerNum");
+	if(NO<playerNum){
+
+
+	//这个函数里面没有调用到数组array[]的值
+	//待解决   数组乱序和函数调用都没有问题  大约是作用域？
+	//？？？？？？
 	if(!change){
 		kuangOne.style.display="flex";
 		kuangTwo.style.display="none";
@@ -98,16 +132,30 @@ function next(){
 		var numbers=document.getElementById("numbers2");
 		numbers.innerHTML=(NO+1);
 		var role=document.getElementById("role");
-		role.innerHTML="角色:"
+
+		role.innerHTML="角色:"+array[NO];//这里没问题
+
+
+
 		NO++;
 
 	}
 	change=!change;
-	var bottomText=document.getElementById("watch");
+	if(NO<playerNum){
+		var bottomText=document.getElementById("watch");
 		bottomText.innerHTML="查看"+(NO+1)+"号身份";
+	}else{
+		var bottomText=document.getElementById("watch");
+		bottomText.innerHTML="法官查看";
+
+	}
 
 
 
+}else{
+
+	window.location.href="peibi.html";
+}
 	// alert(bottomText.innerHTML)
 	// var add=NO+1;
 	// bottomText.innerHTML+=add;
