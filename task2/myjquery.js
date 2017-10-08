5da4c87c6d2a85b735092577ec53cce4cd43e309
pacino9515 <1652258118@qq.com>
2017-10-08 17:23:40 +0800

    update

--- task2/myjquery.js
+++ task2/myjquery.js
@@ -1,20 +1,28 @@
 
 $(document).ready(function(){
 	$(".day").click(function(){
-		$(".day").next(".process").toggle("fast");
+		$(this).next().toggle("fast");
 	});
 
 });
 var rotation = 0;
+$(document).ready(function(){
+	rotation=localStorage.getItem("rotation");
+	for(var i=0;i<rotation;i++){
+		$(".oneday").eq(i).css("background-color","#83b09a");
+		$(".square").eq(i).css("border-right","20px solid #83b09a");
 
+	}
+})
 $(".oneday").click(function(){
-	if($(this).prevAll().length==rotation){
 
+	if($(this).prevAll().length==rotation){
 
 	if(confirm("你确定吗?")){
 		$(this).css("background-color","#83b09a");
 		$(this).children(".square").css("border-right","20px solid #83b09a");
 		rotation++;
+		localStorage.setItem("rotation",rotation);
 	}}
 	else{
 		if($(this).prevAll().length<rotation){
