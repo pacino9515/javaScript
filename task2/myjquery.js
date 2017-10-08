5197c0e95afc9f38469611035bac154d0d170825
pacino9515 <1652258118@qq.com>
2017-10-08 15:43:11 +0800

    update

--- task2/myjquery.js
+++ task2/myjquery.js
@@ -3,13 +3,26 @@ $(document).ready(function(){
 	$(".day").click(function(){
 		$(".day").next(".process").toggle("fast");
 	});
+
 });
+var rotation = 0;
 
 $(".oneday").click(function(){
+	if($(this).prevAll().length==rotation){
+
 
 	if(confirm("你确定吗?")){
 		$(this).css("background-color","#83b09a");
 		$(this).children(".square").css("border-right","20px solid #83b09a");
+		rotation++;
+	}}
+	else{
+		if($(this).prevAll().length<rotation){
+			alert("请进行游戏下一项活动")
+		}else{
+			alert("请按顺序操作")
+		}
+
 	}
 
 });
